4e96176c572bd2359406ca892cf487569985b1fc
Agustin <agustingomez1986@hotmail.com>
2025-05-19 15:57:30 +0200

    Routers post register y login

--- routes/users.py
+++ routes/users.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from schemas import UserCreate
+from crud import create_user, get_user_by_email
+from auth import verify_password, create_access_token, get_db
+
 
 router = APIRouter()
 
-@router.get("/users")
-def obtener_usuarios():
-    return [{"nombre": "Agustín"}, {"nombre": "Lucas"}]
+@router.post("/register")
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email ya registrado")
+    return create_user(db, user)
+
+@router.post("/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, user.email) # get_user_by_email deuelve un User
+    if not db_user or not verify_password(user.email, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Credenciales invalidas")
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
